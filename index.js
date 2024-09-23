4303129a176bba3a786e39475c3aea14459bde26
kudoCodes <cadenbrown@outlook.com>
2024-09-23 14:23:07 -0400

    added ngrok functionality

--- index.js
+++ index.js
@@ -11,6 +11,22 @@ const { token, allChannelID } = require('./config.json');
 const app = express();
 const port = 3000;
 
+// to run server -> NGROK_AUTHTOKEN=2missw90S3jZUXXf3g1qzT4bek8_ChD9ADBRCEPQhoQtySxL node index.js
+
+const http = require('http');
+const ngrok = require('@ngrok/ngrok');
+
+// Create webserver
+http.createServer((req, res) => {
+	res.writeHead(200, { 'Content-Type': 'text/html' });
+	res.end('Congrats you have created an ngrok web server');
+}).listen(8080, () => console.log('Node.js web server at 8080 is running...'));
+
+// Get your endpoint online
+ngrok.connect({ addr: 8080, authtoken_from_env: true })
+	.then(listener => console.log(`Ingress established at: ${listener.url()}`));
+
+
 // Parse incoming GitHub payloads as JSON
 app.use(bodyParser.json());
 
