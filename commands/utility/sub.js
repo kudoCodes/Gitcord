65aea4325df8e98fc88fd057f9d276d50a5cd58e
Mohamadm606 <125500847+Mohamadm606@users.noreply.github.com>
2024-09-28 02:10:06 -0400

    added string option to sub command

--- commands/utility/sub.js
+++ commands/utility/sub.js
@@ -3,8 +3,12 @@ const { SlashCommandBuilder } = require("discord.js");
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('sub')
-        .setDescription("acknowleges your existence"),
+        .setDescription("acknowleges your existence")
+        .addStringOption(option =>
+            option.setName('repolink')
+                .setDescription('The input to echo back')
+                .setRequired(true)),
     async execute(interaction) {
-        await interaction.reply('gloobert!');
+        await interaction.reply(interaction.options.getString('repolink'));
     },
 };
